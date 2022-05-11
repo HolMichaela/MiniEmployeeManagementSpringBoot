777c78a333e52af6482343f0597d6592e702e97f
Ela_Ho <Spam_hat_hier_keine_macht@gmx.de>
2022-05-11 19:36:41 +0200

    Initial commit

---
+++ src/main/java/com/example/miniemployeemanagement/MiniEmployeeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.miniemployeemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniEmployeeManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniEmployeeManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/miniemployeemanagement/MiniEmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miniemployeemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniEmployeeManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
